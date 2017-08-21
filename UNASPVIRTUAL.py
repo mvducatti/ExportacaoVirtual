eedda205fb8f796da43811d89e969f337d249547
IASD\alciomar.hollanda <alciomarhollanda@gmail.com>
2017-08-21 14:49:11 -0300

    Programa para a atualização do UNASP Virtual, atendendo a importação de grupos para qualquer turma do moodle. Realizando a conversão de ANSI para UTF-8

--- UNASPVIRTUAL.py
+++ UNASPVIRTUAL.py
@@ -1,5 +1,7 @@
 import csv
-import re
+import codecs
+import io
+
 
 credentials = {}
 fileGroup = []
@@ -10,21 +12,24 @@ fileEnroll = []
     (Antes do : acontece a identificação do curso se é AD = Administração)
 
 '''
+
 with open('UNASPVIRTUAL.txt', 'r') as f: 
     for line in f:
-        curso, nameGroup = line.strip().split(':') #nome do grupo é igual ao que se encontra no unasp Virtual
-        credentials[curso] = nameGroup #Cria um vetor de importação        
+        curso, nameGroup,cursoEad = line.strip().split(':') #nome do grupo é igual ao que se encontra no unasp Virtual
+        credentials[curso] = [nameGroup,cursoEad] #Cria um vetor de importação        
 
-cursoEad = 'CV-MP'
 
 with open('ENROLL.CSV') as csvfile: #Le arquivo .csv 
     readCSV = csv.reader(csvfile, delimiter=',') 
     next(readCSV)   
     for row in readCSV:
         if(credentials.has_key(row[1])):#Verifica se ele pertence ao grupo do curso EAD CV-MP
-            if(row[3] == 'student'):                
-                lineArquivoGroup = "{},{},{},{}".format(row[0],cursoEad,row[2],credentials[row[1]])    
-                lineArquivoENROLL = "{},{},{},{}".format(row[0],cursoEad,row[2],row[3])
+            if(row[3] == 'student'):
+                nameOfGroup = credentials[row[1]][0] # Nome do grupo que é extraido do arquivo de configuração
+                initialsCourse = credentials[row[1]][1] # sigla do curso a ser importado extraido ... 
+
+                lineArquivoGroup = "{},{},{},{}".format(row[0],initialsCourse,row[2],nameOfGroup)    
+                lineArquivoENROLL = "{},{},{},{}".format(row[0],initialsCourse,row[2],row[3])
                 
                 fileGroup.append(lineArquivoGroup)
         else:
@@ -37,7 +42,7 @@ with open('ENROLL.CSV') as csvfile: #Le arquivo .csv
         # o Row[1] e a informação do curso em código como  HT_AD_AC_2A
         # importante que a abreviação do curso deve ficar na segunda posição 
     
-csvfile = "group_members.csv"
+csvfile = "3_group_members.csv"
 
 with open(csvfile, "w") as text_file:
     text_file.writelines('action,coursekey,userkey,groupkey\n')
@@ -45,9 +50,21 @@ with open(csvfile, "w") as text_file:
         text_file.writelines(f+'\n')
 
 
-csvfile = "ENROLL_Python.csv"
+csvfile = "2_ENROLL_Python.csv"
 
 with open(csvfile, "w") as text_file:
     text_file.writelines('action,coursekey,userkey,rolekey\n')    
     for f  in fileEnroll:
         text_file.writelines(f+'\n')
+
+path =  'USERS.CSV'
+
+
+
+#read input file
+with codecs.open(path, 'r', encoding = 'latin-1') as file:
+  lines = file.read()  
+
+#write output file
+with codecs.open('1_UserPython.CSV', 'w', encoding = 'utf_8_sig') as file:
+  file.write(lines)
